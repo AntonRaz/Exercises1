6d116092ba43a5ab78def21b05e3b7c3c417f4f1
AntonRaz <39989158+AntonRaz@users.noreply.github.com>
2018-06-07 14:34:50 +0300

    Add project files.

---
+++ Exercises/Models/Exercise.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Exercises.Models
+{
+    public class Exercise
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ExerciseDBContext : DbContext
+    {
+        public DbSet<Exercise> Exercises { get; set; }
+    }
+}
